883c9357a5e41de89cb71fa2f549a646554547d4
zvodd <zv.odd.101@gmail.com>
2018-11-14 21:04:19 +0800

    parallel connections

--- proxy_test_script.go
+++ proxy_test_script.go
@@ -5,13 +5,15 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"golang.org/x/net/proxy"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strings"
+	"time"
+
+	"golang.org/x/net/proxy"
 )
 
 var logger = log.New(os.Stderr, "", 0)
@@ -19,6 +21,9 @@ var logger = log.New(os.Stderr, "", 0)
 var AUTH *proxy.Auth = nil
 var TESTURL string = "https://httpbin.org/ip"
 var PORTOVERIDE string = ""
+
+var CONNECTION_TIMEOUT = 8
+var CONNECTION_PARALLEL = 3
 
 type Config struct {
 	User        string `json:"user"`
@@ -57,7 +62,7 @@ func main() {
 
 	var file io.Reader
 	if len(flag.Args()) < 1 {
-		logger.Println("No file name supplied")
+		logger.Println("No input file name supplied.")
 		// file = os.Stdin
 		os.Exit(1)
 	} else {
@@ -70,40 +75,72 @@ func main() {
 
 	}
 
-	parse_input(file)
+	proxies := parse_input(file)
+	// logger.Print(proxies)
+	proccessProxies(proxies)
 }
 
-func parse_input(file io.Reader) {
+func proccessProxies(proxies []string) {
+	batchsize := CONNECTION_PARALLEL
+	lenproxies := len(proxies)
+	if lenproxies < batchsize {
+		batchsize = lenproxies
+	}
+	//do_request(remote, TESTURL)
+	tcount := 0
+	fincount := 0
+	taskchan := make(chan int)
+
+	mytask := func(proxy string, tnum int) {
+		// logger.Printf("Task %d started.", tnum)
+		do_request(proxy, TESTURL)
+		taskchan <- tnum
+	}
+	// spinup initial tasks
+	for ; tcount < batchsize; tcount++ {
+		go mytask(proxies[tcount], tcount)
+	}
+
+	// as tasks finish, replace them
+	for fincount < lenproxies-1 {
+		et := <-taskchan
+		logger.Printf("Task %d finished.", et)
+		fincount++
+		tcount++
+		// until we are out of tasks.
+		if tcount < lenproxies {
+			go mytask(proxies[tcount], tcount)
+		}
+	}
+	close(taskchan)
+}
+
+func parse_input(file io.Reader) (output []string) {
 	scanner := bufio.NewScanner(bufio.NewReader(file))
 	scanner.Split(bufio.ScanLines)
 
 	for i := 0; scanner.Scan(); i++ {
 		values := strings.Split(scanner.Text(), ",")
 		if len(values) < 2 {
-			logger.Println("Bad format on line:", i)
+			logger.Printf("Warning, parsing line %d.\n", i)
 			continue
 		}
 		adds_str := values[1]
 		addresses := strings.Split(adds_str, "|")
 		if len(addresses) < 1 {
-			logger.Println("Bad addresses on line,", i)
+			logger.Printf("Warning, invalid addresses on line %d.\n", i)
 		}
 		for _, remote := range addresses {
 			if len(PORTOVERIDE) > 0 {
 				pair := strings.Split(remote, ":")
 				if len(pair) == 2 {
 					remote = fmt.Sprintf("%s:%s", pair[0], PORTOVERIDE)
+					output = append(output, remote)
 				}
 			}
-			do_request(remote, TESTURL)
-
 		}
-
-		if i == 200 {
-			break
-		}
-
 	}
+	return output
 }
 
 func do_request(address, testUrl string) bool {
@@ -111,32 +148,31 @@ func do_request(address, testUrl string) bool {
 	// create a socks5 dialer
 	dialer, err := proxy.SOCKS5("tcp", address, AUTH, proxy.Direct)
 	if err != nil {
-		// logger.Println("can't connect to the proxy:", err)
+		logger.Println("Error, proxy setup:", err)
 		return false
-	} else {
-		// logger.Println("Connected to the proxy:", address)
 	}
 	// setup a http client
 	httpTransport := &http.Transport{}
-	httpClient := &http.Client{Transport: httpTransport}
+	timeout := time.Duration(CONNECTION_TIMEOUT) * time.Second
+	httpClient := &http.Client{Transport: httpTransport, Timeout: timeout}
 	// set our socks5 as the dialer
 	httpTransport.Dial = dialer.Dial
 	// create a request
 	req, err := http.NewRequest("GET", testUrl, nil)
 	if err != nil {
-		logger.Println("can't create request:", err)
+		logger.Println("Error, request setup:", err)
 		return false
 	}
 	// use the http client to fetch the page
 	resp, err := httpClient.Do(req)
 	if err != nil {
-		logger.Println("can't GET page:", err)
+		logger.Println("Error, request failed:", err)
 		return false
 	}
 	defer resp.Body.Close()
 	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "error reading body:", err)
+		fmt.Fprintln(os.Stderr, "Error, reponse terminated:", err)
 		return false
 	}
 	fmt.Println(string(b))
